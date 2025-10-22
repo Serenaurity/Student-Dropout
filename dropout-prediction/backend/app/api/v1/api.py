8189da08212f905f8f3c00aaa0cd21b6854d45a4
Serendip.au <serendip.au@gmail.com>
2025-10-22 12:31:08 +0700

    Delete dropout-prediction directory

--- dropout-prediction/backend/app/api/v1/api.py
+++
@@ -1,6 +0,0 @@
-﻿from fastapi import APIRouter
-from .endpoints import health, prediction
-
-router = APIRouter()
-router.include_router(health.router, tags=["Health"])
-router.include_router(prediction.router, tags=["Prediction"])
